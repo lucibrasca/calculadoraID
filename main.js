fdb99aa1e649e4ed6f2b224bb5b4214b7ae714ca
Maria Lucila Brasca <lucibrasca@gmail.com>
2023-04-13 23:15:54 -0300

    Inicio

---
+++ main.js
@@ -0,0 +1,120 @@
+let sexo = prompt("Ingrese el sexo (M/F):");
+let fin = false;
+let alimento = "";
+let grupoAlimento = 0;
+let unidadMedida = "";
+let contadorIngestaDiaria = 0;
+
+
+while ( !fin )
+{
+    alimento = prompt("Ingrese el nombre del alimento o ESC para salir:");
+
+    if (alimento.toUpperCase() != "ESC")
+    {
+        grupoAlimento = Number ( prompt("Ingrese el número del grupo al que pertenece:\n\n1- Aceites y grasas\n2- Azúcares y dulces\n3- Bebidas con alcohol o azucaradas\n4- Carnes, Aves y Embutidos\n5- Cereales\n6- Frutas\n7- Frutos secos\n8- Huevos\n9- Lácteos y derivados\n10- Legumbres\n11- Pastelería\n12- Pescados, mariscos y crustáceos\n13- Salsas y condimentos\n14- Verduras y hortalizas"));
+
+        if (grupoAlimento == 3)
+            {
+                unidadMedida = "mililitros";
+            }
+        else
+            {
+                unidadMedida = "gramos";
+            }
+
+        let cantidad = Number (prompt("Ingrese la cantidad ingerida del alimento en "+unidadMedida));
+
+        contadorIngestaDiaria += calcularKCalorias (grupoAlimento , cantidad);
+    }
+    else 
+    {
+        fin = true;
+    }
+}
+
+let caloriasRestantes = 0;
+let porcentaje = 0;
+
+if ((sexo.toUpperCase() == "F") && (contadorIngestaDiaria < 1500))
+{
+    caloriasRestantes = 1500 - contadorIngestaDiaria;
+    porcentaje = (contadorIngestaDiaria*100) / 1500;
+
+    alert ("Su ingesta del día fue de "+contadorIngestaDiaria+" kCal. Esto equivale al "+porcentaje+"% de la IDR.");
+}
+else if ((sexo.toUpperCase() == "M") && (contadorIngestaDiaria < 2000))
+{
+    caloriasRestantes = 2000 - contadorIngestaDiaria;
+    porcentaje = (contadorIngestaDiaria*100) / 2000;
+
+    alert ("Su ingesta del día fue de "+contadorIngestaDiaria+" kCal. Esto equivale al "+porcentaje+"% de la IDR.");
+}
+else if ((sexo.toUpperCase() == "F") && (contadorIngestaDiaria > 1500) && (contadorIngestaDiaria < 2000))
+{
+    alert ("Su ingesta del día fue de "+contadorIngestaDiaria+" kCal. Su ingesta se encuentra dentro de la IDR, esto es entre 1500 y 2000 kCal/día en las mujeres.");
+}
+else if ((sexo.toUpperCase() == "M") && (contadorIngestaDiaria > 2000) && (contadorIngestaDiaria < 2500))
+{
+    alert ("Su ingesta del día fue de "+contadorIngestaDiaria+" kCal. Su ingesta se encuentra dentro de la IDR, esto es entre 2000 y 2500 kCal/día en los hombres.");
+} 
+else
+{
+    alert ("Su ingesta del día fue de "+contadorIngestaDiaria+" kCal. Le recomendamos que reduzca su ingesta diaria.");
+}
+
+
+function calcularKCalorias(grupo, cantidad)
+{
+    let kcal = 0;
+
+        switch (grupo)
+        {
+
+        case 1: //aceites y grasas
+            kcal= 9*cantidad;
+            break;
+        case 2: //azucares y dulces
+            kcal= 5*cantidad;
+            break;
+        case 3: //bebidas con alcohol o azucaradas
+            kcal= 5*(cantidad/10);
+            break;
+        case 4: //Carnes, Aves y Embutidos
+            kcal= 2*cantidad;
+            break;
+        case 5: //Cereales
+            kcal= 4*cantidad;
+            break;
+        case 6: //Frutas
+            kcal= 4*(cantidad/10);
+            break;
+        case 7: //Frutos secos
+            kcal= 6*cantidad;
+            break;
+        case 8: //Huevos
+            kcal= 1*cantidad;
+            break;
+        case 9: //Lácteos y derivados
+            kcal= 2*cantidad;
+            break;
+        case 10: //Legumbres
+            kcal= 3*cantidad;
+            break;
+        case 11: //Pastelería
+            kcal= 9*cantidad;
+            break;
+        case 12: //Pescados, mariscos y crustáceos
+            kcal= 2*cantidad;
+            break;
+        case 13: //Salsas y condimentos
+            kcal= 4*cantidad;
+            break;
+        case 14: //Verduras y hortalizas
+            kcal= 2*(cantidad/10);
+            break;
+
+        }
+
+        return kcal;
+}
